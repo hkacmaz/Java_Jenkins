165551a135309499718b78e2b81912391d28ba63
hkacmaz <halilkacmaz@outlook.com>
2019-08-01 16:09:19 -0700

    Java code edit: for loop edded

--- Hello.java
+++ Hello.java
@@ -2,9 +2,10 @@ public class Hello{
 
 	public static void main(String[] args)
 	{
-
-		
-			System.out.println("Hello Jenkins ...");
+		for (int i=0; i<10; i++)
+		{
+			System.out.println("Hello Jenkins ..." + i);
+		}
 		
 	}
 }
